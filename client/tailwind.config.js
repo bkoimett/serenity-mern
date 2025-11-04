c90f6aaa1e0e281d8d1c4713fab4f7d4246dd070
bkoimett <benkoimet@gmail.com>
2025-11-04 20:22:19 +0300

    feat(styles): refactor index.css and tailwind.config.js for improved styling and color customization

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,23 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f0f9ff",
+          100: "#e0f2fe",
+          500: "#0ea5e9",
+          600: "#0284c7",
+          700: "#0369a1",
+        },
+        teal: {
+          500: "#14b8a6",
+          600: "#0d9488",
+        },
+      },
+    },
   },
   plugins: [],
 };
