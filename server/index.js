f959afd90db2dbf6f92733f1b16bcceb0b90db65
bkoimett <benkoimet@gmail.com>
2025-11-06 00:49:06 +0300

    fix(server): remove commented code to reduce memory used

--- server/index.js
+++ server/index.js
@@ -21,17 +21,6 @@ app.use("/api/blog", blogRoutes);
 // app.use("/api/contact", contactRoutes);
 
 // MongoDB connection
-// mongoose
-//   .connect(
-//     process.env.MONGODB_URI || "mongodb://localhost:27017/serenity-place"
-//   )
-//   .then(() => console.log("✅ MongoDB connected"))
-//   .catch((err) => console.log(err));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(async () => {
